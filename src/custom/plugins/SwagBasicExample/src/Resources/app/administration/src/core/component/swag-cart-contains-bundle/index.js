7813b879c8946c99f13c6db324878d3d256a8e13
Tim Loges <github@timloges.de>
2023-07-26 12:57:57 +0200

    chore: remove unused example plugins

--- src/custom/plugins/SwagBasicExample/src/Resources/app/administration/src/core/component/swag-cart-contains-bundle/index.js
+++
@@ -1,7 +0,0 @@
-import template from './swag-cart-contains-bundle.html.twig';
-
-const { Component } = Shopware;
-
-Component.extend('swag-cart-contains-bundle', 'sw-condition-base', {
-    template
-});
--- src/custom/plugins/SwagBasicExample/src/SwagBasicExample.php
+++
@@ -1,9 +0,0 @@
-<?php declare(strict_types=1);
-
-namespace SwagBasicExample;
-
-use Shopware\Core\Framework\Plugin;
-
-class SwagBasicExample extends Plugin
-{
-}
