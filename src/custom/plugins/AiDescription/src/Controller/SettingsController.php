25d2fd4d6611da77f978e4da3c6cdac8eff5f3b9
Tim Loges <github@timloges.de>
2023-08-27 18:41:47 +0200

    feat: restructure settings controller

--- src/custom/plugins/AiDescription/src/Controller/SettingsController.php
+++ src/custom/plugins/AiDescription/src/Controller/SettingsController.php
@@ -9,34 +9,33 @@ use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\HttpFoundation\Request;
 use AiDescription\Service\ReadingData;
-use Psr\Log\LoggerInterface;
 use Shopware\Core\System\SystemConfig\SystemConfigService;
 
 /**
  * @Route(defaults={"_routeScope"={"api"}})
  */
-class TestController extends AbstractController
+class SettingsController extends AbstractController
 {
-    private $logger;
     private $readingData;
     private SystemConfigService $systemConfigService;
 
-    public function __construct(LoggerInterface $logger, ReadingData $readingData, SystemConfigService $systemConfigService)
+    public function __construct(ReadingData $readingData, SystemConfigService $systemConfigService)
     {
-        $this->logger = $logger;
         $this->readingData = $readingData;
         $this->systemConfigService = $systemConfigService;
 
     }
     /**
-     * @Route("/api/aidescription/test", name="api.action.ai-description.example", methods={"GET"}, defaults={"_routeScope"={"api"}})
+     * @Route("/api/aidescription/exludedProperties", name="api.action.ai-description.excludedProperties", methods={"GET"}, defaults={"_routeScope"={"api"}})
      */
-    public function example(Request $request, Context $contex): JsonResponse
+    public function getExludedProperties(Request $request, Context $contex): JsonResponse
     {
+        $exampleConfig = $this->systemConfigService->get('AiDescription.config.excludedProperties', null);
+        return new JsonResponse(
+            [
+                'excludedProperties' => $exampleConfig
+            ]
+        );
 
-        $products = $this->readingData->readData($contex);
-        // write to log file
-        $this->logger->error('This is a debug message.');
-        return new JsonResponse($products->jsonSerialize());
     }
 }
