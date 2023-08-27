115e94d16ec6ca625eeb98bb994a999f95ef84c4
Tim Loges <github@timloges.de>
2023-08-27 13:33:01 +0200

    feat: plugin page test

--- src/custom/plugins/AiDescription/src/Resources/app/administration/src/module/ai-description/page/index-page/index.js
+++ src/custom/plugins/AiDescription/src/Resources/app/administration/src/module/ai-description/page/index-page/index.js
@@ -2,9 +2,24 @@ import template from "./index-template.html.twig";
 
 Shopware.Component.register("index-page", {
 	template,
+	inject: ["loginService"],
 	metaInfo() {
 		return {
 			title: this.$createTitle(),
 		};
+	},
+	created() {
+		console.log("Hello");
+	},
+	methods: {
+		async callServiceFunction() {
+			const response = await fetch("/api/aidescription/test", {
+				headers: {
+					Authorization: `Bearer ${this.loginService.getToken()}`,
+				},
+			});
+			const data = await response.json();
+			console.log(data);
+		},
 	},
 });
