47cc5efbc9ef1879c71dd1f6a88cfd1475a00d54
Tim Loges <github@timloges.de>
2023-09-20 21:36:40 +0200

    feat: chat api instead of completion

--- src/custom/plugins/AiDescription/src/Service/CallApi.php
+++ src/custom/plugins/AiDescription/src/Service/CallApi.php
@@ -2,50 +2,72 @@
 
 namespace AiDescription\Service;
 
-use Shopware\Core\System\SystemConfig\SystemConfigService;
 use GuzzleHttp\Client;
+use Shopware\Core\System\SystemConfig\SystemConfigService;
 
 class CallApi
 {
     private SystemConfigService $systemConfigService;
 
+    private $configuration = [
+      'max_tokens' => 500,
+      'temperature' => 0.4,
+      'top_p'=> 0.2,
+      'frequency_penalty'=> 0,
+      'presence_penalty'=> 0
+    ];
 
     public function __construct(SystemConfigService $systemConfigService)
     {
         $this->systemConfigService = $systemConfigService;
     }
 
-    public function callApi(String $pompt): string
+    public function callApi(String $instructions, String $initialMessage): string
     {
         //TODO: error if no apikey
         $apikey = $this->systemConfigService->get('AiDescription.config.apikey');
+        if (empty($apikey)) {
+            throw new \Exception('OpenAI API Key is missing. Please add it in the plugin configuration.');
+        }
         $client = new Client([
           // Base URI is used with relative requests
           'base_uri' => 'https://api.openai.com',
-          'timeout'  => 10,
-          'read_timeout' => 10,
+          'timeout'  => 50,
+          'read_timeout' => 50,
       ]);
         /* create bearer authentication header  */
         $headers = [
-            'Authorization' => 'Bearer ' . $apikey,
             'Content-Type' => 'application/json',
+            'Authorization' => 'Bearer ' . $apikey,
         ];
         /* create json body */
         $response = $client->post(
-            '/v1/completions',
+            '/v1/chat/completions',
             [
                 'headers' => $headers,
                 'json' => [
-                  'model' => 'text-davinci-003',
-                  'prompt' => $pompt,
-                  'max_tokens' => 500,
-                  'temperature' => 0.5,
+                  'model' => 'gpt-4',
+                  'messages' => [
+                      [
+                          'role' => 'system',
+                          'content' => $instructions
+                      ],
+                      [
+                          'role' => 'user',
+                          'content' => $initialMessage
+                      ]
+                  ],
+                  ...$this->configuration
                 ]
             ]
         );
         $body = (string) $response->getBody();
 
         return $body;
+    }
+    public function getConfiguration(): array
+    {
+        return $this->configuration;
     }
 
 }
