f2e3763fddb94b73f29dc8f5b652eeb41395c710
Tim Loges <github@timloges.de>
2023-09-22 14:50:19 +0200

    feat: cleanup generate and introduce regenerate

--- src/custom/plugins/AiDescription/src/Controller/GeneratorController.php
+++ src/custom/plugins/AiDescription/src/Controller/GeneratorController.php
@@ -8,9 +8,9 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\HttpFoundation\Request;
-use AiDescription\Service\ReadingData;
 use AiDescription\Service\CallApi;
-use Shopware\Core\System\SystemConfig\SystemConfigService;
+use AiDescription\Service\ComposePrompt;
+use AiDescription\Service\HistoryService;
 
 /**
  * @Route(defaults={"_routeScope"={"api"}})
@@ -18,33 +18,87 @@ use Shopware\Core\System\SystemConfig\SystemConfigService;
 class GeneratorController extends AbstractController
 {
     private $callApi;
+    private $composePrompt;
+    private $history;
 
-    public function __construct(CallApi $callApi)
+    public function __construct(CallApi $callApi, ComposePrompt $composePrompt, HistoryService $historyService)
     {
-        // $this->readingData = $readingData;
-        // $this->systemConfigService = $systemConfigService;
         $this->callApi = $callApi;
+        $this->composePrompt = $composePrompt;
+        $this->history = $historyService;
     }
     /**
      * @Route("/api/aidescription/generateDescription", name="api.action.ai-description.generateDescription", methods={"POST"}, defaults={"_routeScope"={"api"}})
      */
-    public function generateDescription(Request $request, Context $contex): JsonResponse
+    public function generateDescription(Request $request, Context $context): JsonResponse
+    {
+        $requestData = $this->extractData($request);
+        $instructions = $this->composePrompt->composeInstructionsForGeneration($requestData['tonality']);
+        $initialMessage = $this->composePrompt->composeMessageForGeneration($requestData['properties']);
+        $description = $this->getDescription($instructions, $initialMessage);
+
+        $this->saveDescriptionToHistory($context, $requestData['product_id'], $requestData['tonality'], $requestData['properties'], $description, $instructions);
+
+        $currentHistory = $this->history->readHistory($context, $requestData['product_id']);
+        return new JsonResponse(['response' => $description, 'history' => $currentHistory]);
+    }
+
+    /**
+     * @Route("/api/aidescription/regenerateDescription", name="api.action.ai-description.regenerateDescription", methods={"POST"}, defaults={"_routeScope"={"api"}})
+     */
+    public function regenerateDescription(Request $request, Context $context): JsonResponse
+    {
+
+        $requestData = $this->extractData($request);
+        $instructions = $this->composePrompt->composePromptForRephrasing($requestData['tonality']);
+        try {
+            $response = $this->callApi->callApi($instructions, $requestData['description']);
+        } catch (\Exception $e) {
+            return new JsonResponse(['error' => $e->getMessage(), 'response' =>'Fehler beim Generieren der Beschreibung']);
+        }
+        $response = json_decode($response);
+        $description = $response->choices[0]->message->content;
+        $placeholderProperties = [];
+        $this->saveDescriptionToHistory($context, $requestData['product_id'], $requestData['tonality'], $placeholderProperties, $description, $instructions);
+
+        $currentHistory = $this->history->readHistory($context, $requestData['product_id']);
+
+        return new JsonResponse(['response' => $description, 'history' => $currentHistory]);
+
+    }
+    private function saveDescriptionToHistory(Context $context, $productId, $tonality, $properties, $description, $instructions)
+    {
+        $evaluation = "";
+        $used_configuration = $this->callApi->getConfiguration();
+        try {
+            $this->history->writeHistory($context, $productId, $description, $evaluation, $instructions, $used_configuration, $properties, $tonality);
+        } catch (\Exception $e) {
+            return new JsonResponse(['error' => $e->getMessage(), 'response' =>'Fehler beim Speichern der Beschreibung']);
+        }
+    }
+    private function extractData(Request $request): array
     {
         $requestContent = json_decode($request->getContent());
         $tonality = strtolower($requestContent->tonality);
-        $composedString = '';
-        foreach ($requestContent->properties as $property) {
-            $composedString .= "{$property->name}: {$property->options[0]->name}.\n";
-        }
-        $instructions = "Du bist ein Experte für Wein und sollst eine Beschreibung für Wein erstellen. Formuliere den Text $tonality. Die Beschreibung wird in einem Onlineshop genutzt. Die Beschreibung soll nicht länger als 200 Wörter sein. Es sollen nur eine Auswahl der passenden Speisen vorkommen. Nehme die Aromen und Düfte mit in die Beschreibung auf. Hier sind die Informationen zu dem Wein:";
-        $prompt = "$instructions \n$composedString";
-        //TODO: what todo if error and response is no json?
-        $response = $this->callApi->callApi($prompt);
-        return new JsonResponse(
-            [
-                'generateDescription' => $response,
-            ]
-        );
+        $product_id = $requestContent->product_id;
+        $properties = $requestContent->properties;
+        $description = isset($requestContent->description) ? $requestContent->description : null;
 
+        return [
+            'product_id' => $product_id,
+            'tonality' => $tonality,
+            'properties' => $properties,
+            'description' => $description,
+        ];
+    }
+    private function getDescription($instructions, $initialMessage)
+    {
+        try {
+            $response = $this->callApi->callApi($instructions, $initialMessage);
+        } catch (\Exception $e) {
+            return new JsonResponse(['error' => $e->getMessage(), 'response' =>'Fehler beim Generieren der Beschreibung']);
+        }
+        $response = json_decode($response);
+        return $response->choices[0]->message->content;
     }
 }
